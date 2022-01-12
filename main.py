7b1406ed9baebd0bffa65eca5908e697ea53dafa
LBDVCH <lebeddanak@gmail.com>
2022-01-12 09:38:00 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import cv2
+import mediapipe as mp
+
+class HandDetector:
+
+    def __init__(self, mode=False,maxHands=2, detectionCon=0.5, minTrackCon=0.5):
+
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon, min_tracking_confidence = self.minTrackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+        self.lmList = []
+
+    def findHands(self, img, draw=True, flipType=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        allHands = []
+        h, w, c = img.shape
+        if  self.results.multi_hand_landmarks:
+            for handType,handLms in zip(self.results.multi_handedness,self.results.multi_hand_landmarks):
+                myHand={}
+                mylmList = []
+                xList = []
+                yList = []
+                for id, lm in enumerate(handLms.landmark):
+                    px, py = int(lm.x * w), int(lm.y * h)
+                    mylmList.append([px, py])
+                    xList.append(px)
+                    yList.append(py)
+
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), \
+                         bbox[1] + (bbox[3] // 2)
+
+                myHand["lmList"] = mylmList
+                myHand["bbox"] = bbox
+                myHand["center"] =  (cx, cy)
+
+                if flipType:
+                    if handType.classification[0].label =="Right":
+                        myHand["type"] = "Left"
+                    else:
+                        myHand["type"] = "Right"
+                else:myHand["type"] = handType.classification[0].label
+                allHands.append(myHand)
+
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                    if myHand["type"] == "Right":
+                        cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (0, 0, 255), 2)
+                    elif myHand["type"] == "Left":
+                        cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20), (255, 0, ), 2)
+
+
+                    cv2.putText(img,myHand["type"],(bbox[0] - 30, bbox[1] - 30),cv2.FONT_HERSHEY_PLAIN, 1,(0, 0, 0),1)
+        if draw:
+            return allHands,img
+        else:
+            return allHands
+
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(detectionCon=0.8, maxHands=2)
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)  # with draw
+
+        if hands:
+            hand1 = hands[0]
+
+            if len(hands) == 2:
+                hand2 = hands[1]
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
